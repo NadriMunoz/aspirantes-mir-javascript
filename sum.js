0cc0eb7ac1dd4b26c12948d35c55630ec4438e1c
NANCY MUÑOZ <nancymunoz@MacBook-Pro-de-NANCY.local>
2023-05-18 20:30:12 -0500

    solucion a los ejercicios de js

--- sum.js
+++ sum.js
@@ -10,4 +10,4 @@ function suma(numero) {
   // código de prueba
 console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
 console.log(suma(10)) // 55
-console.log(suma(15)) // 120
+console.log(suma(15)) // 120    
